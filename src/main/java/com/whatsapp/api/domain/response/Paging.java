2a901aae5086569b38f7314e14728c13b97c4b26
amit-onappr <amit@onappr.com>
2023-10-16 13:16:09 +0530

    message status

--- src/main/java/com/whatsapp/api/domain/response/Paging.java
+++ src/main/java/com/whatsapp/api/domain/response/Paging.java
@@ -7,9 +7,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * The type Paging.
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record Paging(@JsonProperty("cursors") Cursors cursors,
-
-                     @JsonProperty("next") String next) {
-
+public record Paging(
+        @JsonProperty("cursors") Cursors cursors,
+        @JsonProperty("previous") Cursors previous,
+        @JsonProperty("next") String next) {
 
 }
